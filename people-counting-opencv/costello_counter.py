39abf7c775a099d242dee06c45450cbf40014273
Sorcha Costello <66784199+SorchCostello@users.noreply.github.com>
2020-07-01 12:30:27 +0100

    Update costello_counter.py

--- people-counting-opencv/costello_counter.py
+++ people-counting-opencv/costello_counter.py
@@ -39,18 +39,18 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"sofa", "train", "tvmonitor"]
 
 # load our serialized model from disk
-print("[INFO] Loading Model...")
+print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 # if a video path was not supplied, grab a reference to the webcam
 if not args.get("input", False):
-	print("[INFO] Starting Video Stream... \n[INFO] Get Ready To Smile!")
+	print("[INFO] starting video stream...")
 	vs = VideoStream(src=0).start()
 	time.sleep(2.0)
 
 # otherwise, grab a reference to the video file
 else:
-	print("[INFO] Opening Video File...")
+	print("[INFO] opening video file...")
 	vs = cv2.VideoCapture(args["input"])
 
 # initialize the video writer (we'll instantiate later if need be)
@@ -172,10 +172,10 @@ while True:
 			pos = tracker.get_position()
 
 			# unpack the position object
-			startX = int(pos.left())
-			startY = int(pos.top())
-			endX = int(pos.right())
-			endY = int(pos.bottom())
+			startX = int(pos.top())
+			startY = int(pos.left())
+			endX = int(pos.bottom())
+			endY = int(pos.right())
 
 			# add the bounding box coordinates to the rectangles list
 			rects.append((startX, startY, endX, endY))
@@ -183,7 +183,7 @@ while True:
 	# draw a horizontal line in the center of the frame -- once an
 	# object crosses this line we will determine whether they were
 	# moving 'Right' or 'Left'
-	cv2.line(frame, (W // 2, 0), (W // 2, H), (255, 0, 255), 2)
+	cv2.line(frame, (W // 2, 0), (W // 2, H), (0, 0, 255), 2)
 
 	# use the centroid tracker to associate the (1) old object
 	# centroids with (2) the newly computed object centroids
@@ -202,12 +202,12 @@ while True:
 		# otherwise, there is a trackable object so we can utilize it
 		# to determine direction
 		else:
-			# the difference between the y-coordinate of the *current*
+			# the difference between the x-coordinate of the *current*
 			# centroid and the mean of *previous* centroids will tell
 			# us in which direction the object is moving (negative for
 			# 'Right' and positive for 'Left')
-			y = [c[1] for c in to.centroids]
-			direction = centroid[1] - np.mean(y)
+			x = [c[1] for c in to.centroids]
+			direction = centroid[1] - np.mean(x)
 			to.centroids.append(centroid)
 
 			# check to see if the object has been counted or not
@@ -215,14 +215,14 @@ while True:
 				# if the direction is negative (indicating the object
 				# is moving Right) AND the centroid is above the center
 				# line, count the object
-				if direction < 0 and centroid[1] < H // 2:
+				if direction < 0 and centroid[1] < W // 2:
 					totalRight += 1
 					to.counted = True
 
 				# if the direction is positive (indicating the object
 				# is moving Left) AND the centroid is below the
 				# center line, count the object
-				elif direction > 0 and centroid[1] > H // 2:
+				elif direction > 0 and centroid[1] > W // 2:
 					totalLeft += 1
 					to.counted = True
 
@@ -233,14 +233,14 @@ while True:
 		# object on the output frame
 		text = "ID {}".format(objectID)
 		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+		cv2.circle(frame, (centroid[0], centroid[1]), 4, (255, 255, 255), -1)
 
 	# construct a tuple of information we will be displaying on the
 	# frame
 	info = [
-		("Right", totalRight),
-		("Left", totalLeft),
+		("Left", totalRight),
+		("Right", totalLeft),
 		("Status", status),
 	]
 
@@ -248,7 +248,7 @@ while True:
 	for (i, (k, v)) in enumerate(info):
 		text = "{}: {}".format(k, v)
 		cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+			cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
 
 	# check to see if we should write the frame to disk
 	if writer is not None:
